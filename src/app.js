3e512be7984acede37a63b548505e9ca177880c5
MEETSIDHDHAPURA <meet.mbinfoways@gmail.com>
2025-09-23 15:30:55 +0530

    Update CORS configuration to use environment variable for origin

--- src/app.js
+++ src/app.js
@@ -10,7 +10,12 @@ const TimeSlotRoute = require("./Routes/TimeSlot.route");
 const TechnicianRoute = require("./Routes/Technician.route");
 const ContactRoute = require("./Routes/Contact.route");
 
-app.use(cors());
+console.log(process.env.CORS_ORIGIN);
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN || "*",
+};
+
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "..", "public")));
